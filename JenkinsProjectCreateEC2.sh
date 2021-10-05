b271aa885b254b2aae0c8d00566aaa27f134c69d
MathieuLRAccenture <91892002+MathieuLRAccenture@users.noreply.github.com>
2021-10-05 12:07:21 +0200

    Update JenkinsProjectCreateEC2.sh

--- JenkinsProjectCreateEC2.sh
+++ JenkinsProjectCreateEC2.sh
@@ -1,4 +1,4 @@
 
 # What does this script do: It launches an ec2 instance in my aws account 
 
-aws ec2 run-instances --image-id ami-068d6e9f3899ef107 --count 1 --instance-type t1.micro --key-name EC2Jenkins --security-group-ids sg-060bb982fa401704a --region eu-west-3
+aws ec2 run-instances --image-id ami-068d6e9f3899ef107 --count 1 --instance-type t2.micro --key-name EC2Jenkins --security-group-ids sg-060bb982fa401704a --region eu-west-3
